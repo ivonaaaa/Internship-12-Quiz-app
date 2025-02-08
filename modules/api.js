c31b21ce87eaa5d3ece4331f8eeb0a8b679e9d7f
ivonaaaa <ivonaa.e@gmail.com>
2025-02-08 01:20:57 +0100

    Add api.js with proper fetching of the quiz questions

---
+++ modules/api.js
@@ -0,0 +1,25 @@
+async function fetchQuestions(category, difficulty, type) {
+  const baseUrl = "https://opentdb.com/api.php";
+
+  const queryParams = {};
+  if (category) queryParams.category = category;
+  if (difficulty) queryParams.difficulty = difficulty;
+  if (type) queryParams.type = type;
+
+  queryParams.amount = 5;
+  const params = new URLSearchParams(queryParams);
+
+  return fetch(`${baseUrl}?${params.toString()}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.response_code !== 0) {
+        throw new Error("No questions available for the selected parameters.");
+      }
+      return data.results;
+    })
+    .catch((error) => {
+      console.error(`An error ocurred: ${error.message}`);
+      throw error;
+    });
+}
+export { fetchQuestions };
