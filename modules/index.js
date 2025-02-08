d89f90460b435b149a8214019f0ba3b316303e2a
ivonaaaa <ivonaa.e@gmail.com>
2025-02-08 01:44:44 +0100

    Add index.js with basic functionality logic; Add separate button in HTML

---
+++ modules/index.js
@@ -0,0 +1,26 @@
+import { fetchQuestions } from "./api.js";
+
+document
+  .getElementById("quiz-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const category = document.getElementById("category").value;
+    const difficulty = document.getElementById("difficulty").value;
+    const type = document.getElementById("type").value;
+
+    try {
+      const questions = await fetchQuestions(category, difficulty, type);
+      document.getElementById("quiz-form").style.display = "none";
+      document.getElementById("start-quiz-btn").style.display = "block";
+
+      document
+        .getElementById("start-quiz-btn")
+        .addEventListener("click", () => {
+          document.getElementById("start-quiz-btn").style.display = "none";
+          //ode pozvat kasnije questions i file za kviz?
+        });
+    } catch (error) {
+      alert("An error occurred while fetching questions. Please try again.");
+    }
+  });
